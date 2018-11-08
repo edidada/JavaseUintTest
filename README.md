93b3b7f34e1e6f0a873339236fb6774a2bb8f105
edidada <1664884095@qq.com>
2018-11-08 11:03:35 +0800

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javase_Uint_Test
+
+JavaseUintTest
---
+++ javaseuinttest/src/cn/edidada/javase/Test.java
@@ -0,0 +1,15 @@
+package cn.edidada.javase;
+
+import java.nio.channels.Selector;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+public class Test {
+
+	public static void main(String args[]){
+		System.out.println("Hello javase");
+		Selector mSelector;
+		LongAdder mLongAdder;
+		AtomicLong mAtomicLong;
+	}
+}
